3625cabb5d884b196a090db453b993aed5430e29
Tate Wilson <tatewilson4@gmail.com>
2018-03-22 13:35:56 -0600

    seed

--- server.js
+++ server.js
@@ -28,15 +28,108 @@ app.use(session({
   saveUninitialized: false
 }));
 
-// app.get('/events/seedEvents', (req, res) => {
-//
-//   Events.create(seed, (err, createdEvents) => {
-//     // logs created users
-//     console.log(createdEvents);
-//     // redirects to index
-//     res.redirect('/events');
-//   });
-// });
+app.get('/events/seed' , (req, res) => {
+  Event.create([
+    {
+      title: "Mount Elbert: 14,433 feet - Class 2",
+      img: "https://gearjunkie.com/legacy/images/19992.jpg",
+      description: "Colorado’s highest summit is a grandfatherly gentle giant, at least by Rocky Mountains standards. Elbert’s large, rounded dome is easily accessed by several trails, all of which begin in vanilla-scented pine forests and emerge from treeline with stunning views of the Sawatch Range. That’s not to say it’s a walk in the park — there’s a matter of 4,400 feet of elevation gain over 4.5 miles to deal with — but the paths are well maintained and easy to follow. Reach the top and you’ll be in the heart of the highest mountain range in the lower 48.",
+      rating: 5
+    },
+    {
+      title: "Capitol Peak: 14,130 feet – Class 4",
+      img: "https://gearjunkie.com/legacy/images/19988.jpg",
+      description: "Among the most challenging 14ers, Capitol Peak is tough for the right reasons — solid rock, heart-in-your-throat exposure and tricky but fair route finding. It’s an 18-mile round trip, and that’s part of the joy. A long approach along Capitol Creek passes through gorgeous aspen forests and ends at Capitol Lake, where the real work begins. After navigating a steep pass, honest class 4 rock makes for a spectacular path to the summit, including the notorious knife edge traverse. Savvy climbers know the top is only the half-way point — the way down is just as fun and demanding as the ascent",
+      rating: 5
+    },
+    {
+      title: "San Luis Peak: 14,014 feet - Class 2",
+      img: "https://gearjunkie.com/legacy/images/19994.jpg",
+      description: "San Luis Peak is located in a remote portion of the San Juan Mountains near the town of Gunnison, Colo., — smack dab in the middle of the Rocky Mountains. Isolation is part of its charm. A summer venture to San Luis follows several peaceful creeks past vibrant fields of wildflowers. Views are expansive and as the alpine grasses yield to the rocky terrain of the summit block, the trail remains intact and easy to navigate. The summit views are pristine, rolling and nearly absent of any man-made landmarks.",
+      rating: 5
+    },
+    {
+      title: "Oktoberfest",
+      img: "http://diningoutwp.funjuczse.maxcdn-edge.com/denverboulder/wp-content/uploads/sites/13/2016/09/Denveroktoberfest.jpg",
+      description: "Fred and Hertha Thomas owned two shops in Larimer Square (Cafe Promenade and Boutique Int’l). In 1969, they decided to honor their favorite tradition in front of the Cafe, and quietly tapped a keg and served some pretzels and white radishes. Until 2007, the event was held on the 14th block of Larimer St. and since it has been hosted at 20th and Larimer in historic Denver, Colorado. The Denver Oktoberfest attracts over 350,000 people during the six-day festival and it was quoted in Maxim Magazine and USA Today as “The Best Oktoberfest” in the United States and one of the country’s biggest and longest running traditions.  It’s also one of the largest beer festivals in the United States based on consumption because Denver drinks beer really well.",
+      rating: 5
+    },
+    {
+      title: "Steamboat",
+      img: "https://www.coloradoski.com/sites/default/files/wp-content/uploads/2015/03/Steamaboat_Fireworks_SPD4.jpg",
+      description: "A cowboy ski town that holds the record for being home to more Winter Olympic athletes than anywhere else in the United States, Steamboat Springs is a hotbed of snowsports culture! The oldest ski hill in Colorado opened here in 1915, and since then, the newer and much larger Steamboat Ski Resort has opened with 2,965 ac of diverse ski terrain across an entire mountain range. The town is also home to geothermal hot springs where you can soak those weary muscles after a hard day on the slopes!",
+      rating: 5
+    },
+    {
+      title: "Keystone",
+      img: "http://coloradoskiauthority.com/assets/images/Ski-Resorts/Keystone/Keystone-Village-Night-Skiing.jpg",
+      description: "Keystone treads the middle ground between luxury ski resorts like Vail and bustling ski towns like Breckenridge, offering an affordable and family-friendly ski experience while remaining competitive in other aspects. Night skiing is one of them, with the largest number of trails in the state lit up after dark here, which solidifies Keystone’s claim of having Colorado’s longest ski day! It also has a fantastic cat ski operation that costs just $10 a ride, opening up huge swathes of powder-filled glades and bowls to tear through.",
+      rating: 5
+    },
+    {
+      title: "Arapahoe Basin",
+      img: "http://www.abasin.mobi/Abasin/image-library/interior-focal-points/terrain_status_2014a.jpg",
+      description: "Arapahoe Basin, A-Basin, The Legend, however you want to call this ski resort, its reputation precedes itself. Shrugging off the idea that ski resorts need to offer a multitude of facilities and amenities, it instead focuses solely on riding the mountain. This draws a younger crowd that like to drink beers and rip through the steeps - giving the A-basin a very authentic local flavor. Thanks to its north facing slopes and high elevation, Arapahoe Basin enjoys one of the longest ski seasons in Colorado, opening in October and sometimes staying open until July!",
+      rating: 5
+    },
+    {
+      title: "Decadence",
+      img: "https://i0.wp.com/substreammagazine.com/wp-content/uploads/2016/12/decadence-nye1-1500x1000.jpg?fit=1074%2C716&ssl=1",
+      description: "Citizens of Decadence! On December 30th and 31st, we invite you to once again cast aside your worldly identities and carouse in our city of boundless indulgence. For two exhilarating nights, prepare to transcend time and reality as we are transported into the City Beyond Tomorrow! All fortunate enough to populate this futuristic utopia will experience unparalleled levels of excess as the magnificent Colorado Convention Center is transformed into a revolutionary metropolis that pushes the limits of the unknown. Join tens of thousands of music lovers from around the globe and make mischief in our adults-only playground that eclipses the very heights of extravagance. Enter and catch a glimpse of the extraordinary destiny that exists beyond tomorrow in our city filled with jaw-dropping production, exquisite decor, enchanting performers and unimaginable innovations just waiting to be discovered.",
+      rating: 5
+    },
+    {
+      title: "Gem Lake",
+      img: "https://303magazine.com/wp-content/uploads/2017/04/wildflowers-RMNP-1024x683.jpg",
+      description: "This somewhat difficult hike features a small, spring-fed mountain lake and it’s not hard to figure out why it’s named gem. Beautiful mountain views of the Continental Divide and scenes of Estes Park and Longs Peak surround you as well as unique rock formations on this glacial moraine. Pretty flowers are scattered all over, making it a great hike to catch some early season blossoms.",
+      rating: 5
+    },
+    {
+      title: "Mayflower Gulch",
+      img: "https://303magazine.com/wp-content/uploads/2017/04/mayflower-gulch-all-trails-2-1024x768.jpg",
+      description: "If you want to spin around in a gorgeous green mountain field full of flowers like Maria does in The Sound of Music, then hiking in Mayflower Gulch is your best bet. This trail close to Breck and Copper Mountain is dotted with yellow, purple and red flowers that look gorgeous next to the jagged mountain backdrop and old cabins of the Boston Mine. The hills are clearly alive here.",
+      rating: 5
+    },
+    {
+      title: "Bighorn Creek",
+      img: "https://303magazine.com/wp-content/uploads/2017/04/bighorn-creek-all-trails-1024x769.jpg",
+      description: "Although this hike isn’t the most popular near Vail since it doesn’t have a lake, Bighorn Creek is worth the visit since it has the prettiest valleys with an abundance of blooms. The trail makes for an easy day hike and has some of the most beautiful meadows with flowers dotting the path the entire way.",
+      rating: 5
+    },
+    {
+      title: "Royal Gorge",
+      img: "https://www.americanadventure.com/wp-content/uploads/2017/03/royal-gorge-white-water-rafting-Colorado-768x513.jpg",
+      description: "The Royal Gorge is one of the Centennial State’s most popular attractions and home to one of the world’s highest suspension bridges – 1,270 feet across and 956 feet above the water. Outside of Canyon City and near Colorado Springs and Pueblo, the Gorge feels middle-of-nowhere, yet is easily accessible.",
+      rating: 5
+    },
+    {
+      title: "Red Rocks Amphitheatre",
+      img: "http://res.cloudinary.com/simpleview/image/upload/v1492635589/clients/denver/Courtesy_of_Denver_Arts_Venues_photo_by_Stevie_Crecelius_54d857ea-c300-4659-a529-2f9b6c7af129.jpg",
+      description: "Red Rocks Amphitheatre is a rock structure near Morrison, Colorado, 10 miles (16 km) west of Denver, where concerts are given in the open-air amphitheatre. There is a large, tilted, disc-shaped rock behind the stage, a huge vertical rock angled outwards from stage right, several large outcrops angled outwards from stage left and a seating area for up to 9,525 people in between. At its height, the amphitheatre sits at 6,450 feet, and the surrounding Red Rocks Park covers 868 acres.",
+      rating: 5
+    },
+    {
+      title: "Coors Field",
+      img: "https://images1.westword.com/imager/u/745x420/10048880/https_3a_2f_2fcdn.evbuc.com_2fimages_2f41537366_2f244605609911_2f1_2foriginal.jpg_h_200_w_450_rect_0_2c90_2c640_2c320_s_50e98e6b395eed3631b440697464bcaa",
+      description: "Coors Field is a baseball park located in downtown Denver, Colorado. It is the home field of the Colorado Rockies, the city's Major League Baseball MLB franchise. If you don't like baseball, no worries because you can get tickets as low as $4.00. The recently added rooftop bar is a great place to enjoy Colorado's scenery while having a beer with your buddies.",
+      rating: 5
+    },
+    {
+      title: "Hanging Lake",
+      img: "https://images.theoutbound.com/adventures/100794/assets/1411411485986?w=1200&h=630&fit=crop&auto=format",
+      description: "Hanging Lake is a geologic wonder and is popular with travelers for its awe-inspiring beauty and gentle Colorado waterfals. Suspended on the edge of Glenwood Canyon's cliffs, the clear turquoise lake and the waterfalls that spill into it are a breathtaking sight after the rigorous uphill climb. This precious natural wonder is one we all must work together to protect by respecting the rules.",
+      rating: 5
+    },
+    {
+      title: "Pikes Peak",
+      img: "https://www.visitcos.com/images/placeholder.jpg",
+      description: "Pikes Peak, located in the Rocky Mountains, is one of the most famous summits in the United States. The 14,115 ft fourteener—a mountain exceeding 14,000 ft above sea level—stands 8,000 ft high over what is known as the Pikes Peak region. This region includes many popular mountain towns at its base such as Colorado Springs, Manitou Springs, Cripple Creek, and Cañon City.",
+      rating: 5
+    }
+  ], (err, data) => {
+    res.redirect('/events');
+  })
+});
 
 
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/events'
